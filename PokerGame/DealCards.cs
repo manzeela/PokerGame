1a1598995b4e4143039a35d6748354138a5d80f1
Manjila Khanal <memanzee@gmail.com>
2021-04-14 11:27:34 +1000

    few changes on code to make it clean

--- PokerGame/DealCards.cs
+++ PokerGame/DealCards.cs
@@ -8,23 +8,34 @@ namespace PokerGame
 {
     class DealCards : DeckOfCards
     {
+        private Card[][] Hands = new Card[4][];
+        private Card[][] SortedHand= new Card[4][];
         // define the hand of four players
         private Card[] firstHand, secondHand, thirdHand, fourthHand;
         // define the sorted hand for four players
         private Card[] sortedFirstHand, sortedSecondHand, sortedThirdHand, sortedFourthHand;
-        
-
-        public DealCards() {
-            firstHand = new Card[5];
-            secondHand = new Card[5];
-            thirdHand = new Card[5];
-            fourthHand = new Card[5];
-            sortedFirstHand = new Card[5];
-            sortedSecondHand = new Card[5];
-            sortedThirdHand = new Card[5];
-            sortedFourthHand = new Card[5];
+        public DealCards()
+        {
+          for(int i = 0; i < 4; i++)
+            {
+                Hands[i] = new Card[5];
+                SortedHand[i] = new Card[5];
+            }
 
         }
+
+
+        //public DealCards() {
+        //    firstHand = new Card[5];
+        //    secondHand = new Card[5];
+        //    thirdHand = new Card[5];
+        //    fourthHand = new Card[5];
+        //    sortedFirstHand = new Card[5];
+        //    sortedSecondHand = new Card[5];
+        //    sortedThirdHand = new Card[5];
+        //    sortedFourthHand = new Card[5];
+
+        //}
 
         public void deal() {
             setDeck(); //create the deck of cards
@@ -34,112 +45,85 @@ namespace PokerGame
             displayCards(); // display the cards in each hand
             evaluateHands(); //evaluate the cards in each hand and determine winner
         }
-
-
         public void getHand()
         {
-            //5 unique cards for first hand
-            for (int i = 0; i < 5; i++) {
-                firstHand[i] = getDeck[i];
-            }
 
-            //5 unique cards for second hand
-            for (int i = 5; i < 10; i++)
+            int k = 0;
+            for (int i = 0; i < 4; i++)
             {
-                secondHand[i-5] = getDeck[i];
-            }
+                for (int j = 0;  j < 5; j++)
+                {
+                    Hands[i][j] = getDeck[k];
+                    k ++;
+                }
 
-            //5 unique cards for third hand
-            for (int i = 10; i < 15; i++)
-            {
-                thirdHand[i-10] = getDeck[i];
             }
-
-            //5 unique cards for fourth hand
-            for (int i = 15; i < 20; i++)
-            {
-                fourthHand[i-15] = getDeck[i];
-            }
+           
         }
+
+
+        //public void getHand()
+        //{
+        //    //5 unique cards for first hand
+        //    for (int i = 0; i < 5; i++) {
+        //        firstHand[i] = getDeck[i];
+        //    }
+
+        //    //5 unique cards for second hand
+        //    for (int i = 5; i < 10; i++)
+        //    {
+        //        secondHand[i-5] = getDeck[i];
+        //    }
+
+        //    //5 unique cards for third hand
+        //    for (int i = 10; i < 15; i++)
+        //    {
+        //        thirdHand[i-10] = getDeck[i];
+        //    }
+
+        //    //5 unique cards for fourth hand
+        //    for (int i = 15; i < 20; i++)
+        //    {
+        //        fourthHand[i-15] = getDeck[i];
+        //    }
+        //}
 
         public void sortCards()
         {
-            //query that stores the sorted card for each hand in an array
-            var firstQuery = firstHand.OrderBy(hand => hand.cFace);
-            var secondQuery = secondHand.OrderBy(hand => hand.cFace);
-            var thirdQuery = thirdHand.OrderBy(hand => hand.cFace);
-            var fourthQuery = fourthHand.OrderBy(hand => hand.cFace);
 
-            //storing the sorted cards in the sorted hands for each player
-            for (int i = 0; i < firstQuery.ToList().Count; i++) {
-                sortedFirstHand[i] = (firstQuery.ToList())[i];
-            }
-
-           
-            for (int i = 0; i < secondQuery.ToList().Count; i++)
+            for (int i = 0; i < 4; i++)
             {
-                sortedSecondHand[i] = (secondQuery.ToList())[i];
+                var query = Hands[i].OrderBy(hand => hand.cFace);
+                for (int j = 0; j < query.ToList().Count; j++)
+                {
+                    SortedHand[i][j] = (query.ToList())[j];
+                }
             }
 
-           
-            for (int i = 0; i < thirdQuery.ToList().Count; i++)
-            {
-                sortedThirdHand[i] = (thirdQuery.ToList())[i];
-            }
-
-           
-            for (int i = 0; i < fourthQuery.ToList().Count; i++)
-            {
-                sortedFourthHand[i] = (fourthQuery.ToList())[i];
-            }
         }
 
         private void displayCards()
         {
-            //display first hand
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("First Hand");
-            Console.ResetColor();
-            for (int i = 0; i < 5; i++) 
+            for(int i = 0; i < 4; i++)
             {
-                Console.WriteLine(sortedFirstHand[i].cFace + " of " + sortedFirstHand[i].cSuit);
-            }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(i.ToString() +" hand" );
+                Console.ResetColor();
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine(SortedHand[i][j].cFace + " of " + SortedHand[i][j].cSuit);
+                }
 
-            //display second hand
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("\n\nSecond Hand");
-            Console.ResetColor();
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine(sortedSecondHand[i].cFace + " of " + sortedSecondHand[i].cSuit);
             }
-
-            //display third hand
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("\n\nThird Hand");
-            Console.ResetColor();
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine(sortedThirdHand[i].cFace + " of " + sortedThirdHand[i].cSuit);
-            }
-
-            //display fourth hand
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("\n\nFourth Hand");
-            Console.ResetColor();
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine(sortedFourthHand[i].cFace + " of " + sortedFourthHand[i].cSuit);
-            }
+            
         }
 
         public void evaluateHands() {
             //create player's and computer's evaluation objects by passing sortedhand for each player to constructor
-
-            EvaluateHands firstHandEvaluator = new EvaluateHands(sortedFirstHand);
-            EvaluateHands secondhandEvaluator = new EvaluateHands(sortedSecondHand);
-            EvaluateHands thirdHandEvaluator = new EvaluateHands(sortedThirdHand);
-            EvaluateHands fourthHandEvaluator = new EvaluateHands(sortedFourthHand);
+            EvaluateHands firstHandEvaluator = new EvaluateHands(SortedHand[0]);
+            EvaluateHands secondhandEvaluator = new EvaluateHands(SortedHand[1]);
+            EvaluateHands thirdHandEvaluator = new EvaluateHands(SortedHand[2]);
+            EvaluateHands fourthHandEvaluator = new EvaluateHands(SortedHand[3]);
 
             //get evaluated hand for all players
 
@@ -148,7 +132,8 @@ namespace PokerGame
             hand thirdHand = thirdHandEvaluator.evaluateHand();
             hand fourthHand = fourthHandEvaluator.evaluateHand();
 
-            
+
+
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\n\n\nResult: ");
             Console.ResetColor();
@@ -176,7 +161,7 @@ namespace PokerGame
             }
             else if (fourthHand > secondHand && fourthHand > firstHand && fourthHand > thirdHand)
             {
-                Console.WriteLine("Third Hand wins!");
+                Console.WriteLine("Fourth Hand wins!");
             }
             else // if the hands are same evaluate values
             {
